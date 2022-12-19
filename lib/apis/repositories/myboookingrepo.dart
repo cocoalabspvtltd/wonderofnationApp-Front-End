2497dbd54a687fa2af016cbf37da95cb0c6b7238
anjanamurugan41 <anjanaglobosoft@gmail.com>
2022-12-19 11:39:23 +0530

    joined matches

--- lib/apis/repositories/myboookingrepo.dart
+++ lib/apis/repositories/myboookingrepo.dart
@@ -35,3 +35,33 @@ class MyOrdersRepositoryUser {
 
   }
 }
+
+class MyOrdersRepositoryUser1 {
+  late WebApiProvider apiProvider;
+
+  MyOrdersRepositoryUser() {
+    apiProvider = new WebApiProvider();
+  }
+
+  Future<MyBookingModel> getAllOrdersList(int perPage, int page) async {
+    FormData formData = FormData.fromMap({
+      "page":page,
+      "per_page":perPage,
+      "match_type":2
+    });
+    final response = await apiProvider
+        .getJsonInstance()
+        .post(baseurl+"bookings/upcoming", data: formData,
+        options: Options(
+            headers: {
+              'Accept':'application/json',
+              'Authorization':"Bearer " + TOKEN,
+            }
+        )
+
+    );
+    print("->>>>>>>>>>>${response.data}");
+    return MyBookingModel.fromJson(response.data);
+
+  }
+}
